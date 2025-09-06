157a9f35b07f9fc151619ec07930f893a1d225cc
khalid <palahuddinalkhalid@gmail.com>
2025-09-06 11:07:47 +0800

    fixed: fixed inventory

--- src/models/Inventory.js
+++ src/models/Inventory.js
@@ -1,18 +1,16 @@
 import { DataTypes } from "sequelize";
 import sequelize from "../config/db.sequelize.config.js";
-import Product from "../models/product.js";
+import Product from "./product.js";
 
-const Inventory = sequelize.define(
-    "Inventory",
-    {
-        quantity: { type: DataTypes.INTEGER, defaultValue: 0 },
-        customer_id: { type: DataTypes.INTEGER, allowNull: false }, // new field
+const Inventory = sequelize.define("Inventory", {
+    quantity: {
+        type: DataTypes.INTEGER,
+        defaultValue: 0,
     },
-    {
-        tableName: "Inventories",
-        timestamps: true,
-    }
-);
+}, {
+    tableName: "Inventories",
+    timestamps: true,
+});
 
 // Associations
 Product.hasOne(Inventory, { foreignKey: "product_id", onDelete: "CASCADE" });
